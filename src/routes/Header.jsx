b4a01d696420638b41dfb8e6723e7c30f05171c4
THATOMAS <chachumashori@gmail.com>
2022-07-21 23:53:10 +0200

    Finished styling the header searchbar section

--- src/routes/Header.jsx
+++ src/routes/Header.jsx
@@ -2,38 +2,35 @@ import React from "react"
 import styled from "styled-components"
 
 const searchIcon = require("../assets/MarketPlace/searchIcon.png")
+const close = require("../assets/MarketPlace/close.png")
+
 
 const HeaderContainer = styled.article`
-height:15vh;
+height:18vh;
 width:100vw;
 background:#E7E7E7;
 display:flex;
-flex-direction:column
+flex-direction:column;
+justify-content:flex-end;
+align-items:center;
 `
 
-const HeadingAndSearchBarBox = styled.div`
-height:40%;
-width:100vw;
-display:flex;
-align-items:center;
-justify-content:space-between;
-`
+
 const Heading = styled.h1`
 font-family:Montaga;
 color:black;
-font-size:35px;
+font-size:30px;
 font-weight:lighter;
-
+margin-bottom:5px
 `
 
 const Form = styled.form`
 display:flex;
-margin-right:50px
 `
 
 const Input = styled.input`
 
-width:350px;
+width:500px;
 display:flex;
 border:5px solid #B87333;
 background:#E7E7E7;
@@ -56,7 +53,7 @@ cursor:pointer;
 const PreferencesBox = styled.div`
 height:60%;
 width:100vw;
-background:red;
+// background:red;
 display:flex;
 align-items:center;
 justify-content:space-around;
@@ -65,7 +62,7 @@ justify-content:space-around;
 const SinglePreferenceBox = styled.div`
 height:70px;
 width:200px;
-background:blue;
+// background:blue;
 display:flex;
 flex-direction:column;
 align-items:center;
@@ -78,7 +75,7 @@ color:black;
 font-size:25px;
 height:40%;
 width:100%;
-background:green;
+// background:green;
 display:flex;
 align-items:center;
 justify-content:center;
@@ -88,43 +85,83 @@ justify-content:center;
 const Select = styled.select`
 height:60%;
 width:100%;
-background:yellow;
+// background:yellow;
 display:flex;
 align-items:center;
 justify-content:center;
-
-
+background:none;
+text-align:center;
+font-family:Montaga;
+color:black;
+font-size:25px;
+background:white;
 `
 const Option = styled.option`
 font-family:Montaga;
 color:black;
-font-size:22px;
+font-size:25px;
 outline:none;
-background:#B87333
+background:white;
+&:hover{
+	background:#B87333
 }
-
 `
-
 
 const Header = ()=>{
 	return(<HeaderContainer>
-		<HeadingAndSearchBarBox>
+
 			<Heading>Find your dream property here</Heading>
 			<Form >
 				<Input type="text" name="search" />
 				<Button type="submit"/>
 			</Form>
-		</HeadingAndSearchBarBox>
+
 		<PreferencesBox>
 			<SinglePreferenceBox>
-				<Label>Property Type:</Label>
+				<Label>Property type:</Label>
 				<Select >
 					<Option>Houses</Option>
 					<Option>Apartments</Option>
 					<Option>Commune</Option>		
-				</Select>				
+				</Select>
 			</SinglePreferenceBox>
-			
+							
+			<SinglePreferenceBox>
+				<Label>Max price:</Label>
+				<Select >
+					<Option>R 4 m</Option>
+					<Option>R 5 m</Option>
+					<Option>R 6 m</Option>		
+				</Select>
+			</SinglePreferenceBox>
+							
+			<SinglePreferenceBox>
+				<Label>Min price:</Label>
+				<Select >
+					<Option>R 600 K</Option>
+					<Option>R 500 K</Option>
+					<Option>R 400 K</Option>		
+				</Select>
+
+
+			</SinglePreferenceBox>
+							
+	
+						<SinglePreferenceBox>
+				<Label>Bedrooms:</Label>
+				<Select >
+
+					<Option>1</Option>
+					<Option>2</Option>
+					<Option>3</Option>
+					<Option>4</Option>
+					<Option>5</Option>
+					<Option>6</Option>		
+				</Select>
+
+				
+			</SinglePreferenceBox>
+							
 
 		</PreferencesBox>
 
